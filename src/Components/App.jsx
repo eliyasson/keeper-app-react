3a151e679a176c978a134955f12752c4abadafce
eliyas <eliyasson11@gmail.com>
2024-03-15 01:00:37 +0200

    add button

---
+++ src/Components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  return (
+    <div >
+      <header>
+        <h1>keeper</h1>
+      </header>
+      <form>
+        <input type="text" placeholder="Title" /><br />
+        <input type="text" placeholder="Content" /><br />
+        <button>Save</button>
+      </form>
+      <footer>
+        @eliyas
+      </footer>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM  from 'react-dom';
+import App from './Components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <>
     <App />
-  </React.StrictMode>
+  </>,
+document.getElementById('root')
 );
 
