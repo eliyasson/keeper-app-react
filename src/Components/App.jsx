b26d7768226abec4613b52e1b0e40378a203d755
eliyas <eliyasson11@gmail.com>
2024-04-12 14:58:03 +0300

    Update the form

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,20 +3,12 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
 import notes from "../Note";
-import Content from "./Content";
 
 function App() {
   return (
     <div >
       <Header />
       <Note />
-      {notes.map(notes => (
-         <Content 
-          key={notes.key}
-          title={notes.title}
-          content={notes.content}
-        />
-      ))}
      
       <Footer />
     </div>
--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,22 +1,76 @@
-import React from "react";
-import Content from "./Content";
-import notes from "../Note";
+import React, {useState} from "react";
 
-function AddNote(e) {
-   
-        e.preventDefault();
-        console.log('You clicked submit.');
-      
-}
+
 function Note(props) {
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+    email: ""
+  });
+
+    function handleChange(event) {
+        const {value, name} = event.target;
+
+      //const newValue = event.target.value;
+      //const inputName = event.target.name;
+      
+      setFullName(prevValue => {
+        return {
+            ...prevValue,
+        [name]: value
+        }
+      })
+
+      /* setFullName(prevValue => {
+        if(name === "fname") {
+            return {
+                ...prevValue,
+                fname: value 
+            }
+        } else if (name === "lname") {
+            return {
+                ...prevValue,
+                lname: value
+            }
+        } else if (name === "email") {
+            return {
+                ...prevValue,
+                email: value
+            }
+        }
+      }); */
+    }
     return (
-      <div>
-        <form onSubmit={AddNote}>
-          <input type="text" placeholder="Title" /><br />
-          <input type="text" placeholder="Content" /><br />
-          <button type="submit" >Save</button>
-        </form>
-      </div>
+        <div className="container">
+            <h1>Todo List</h1>
+            
+            <form  className="form">
+                <input 
+                    name="fname" 
+                    onChange={handleChange}  type="text" 
+                    placeholder="First Name"  
+                    value={fullName.fname}
+                />
+
+                <input 
+                    onChange={handleChange} 
+                    name="lname" type="text" 
+                    placeholder="Last Name" 
+                    value={fullName.lname}
+                />
+                <input 
+                    onChange={handleChange} 
+                    name="email" type="text" 
+                    placeholder="Email" 
+                    value={fullName.email}
+                />
+
+                <button type="submit">Submit</button>
+            </form>
+            <h2>Hello, {fullName.fname} {fullName.lname}</h2>
+            <h3>{fullName.email}</h3>
+          
+        </div>
     )
 }
 export default Note;
